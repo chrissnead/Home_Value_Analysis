e6c1b9f84d368143ed6da7644969d458b1814db9
klcollins503 <95047485+klcollins503@users.noreply.github.com>
2022-05-23 19:17:35 -0800

    Add files via upload

--- model/request.py
+++ model/request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/results'
-r = requests.post(url,json={'Square Feet':1400, 'Zip Mean HHI':75000, 'Zip Pop Density':200,'Lot Size':2400,'Year Built':1991})
+r = requests.post(url,json={'Square Feet':1400, 'Zip Code':97214})
 
 print(r.json())
